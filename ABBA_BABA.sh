3a70c6bdc83a4813f112d2f2a36748bff106e2e7
Tobias Lanzl <60113807+LanzlTobias@users.noreply.github.com>
2020-01-24 14:25:06 +0100

    Update ABBA_BABA.sh

--- ABBA_BABA.sh
+++ ABBA_BABA.sh
@@ -1,31 +1,53 @@
+## Script for running the ABBA_BABA test
+#First argument is the VCF file (gzipped or not)
+#Second argument is the file containing the paths to all poporder-files. Each path has to be given in a new line.
+#Example of a poporder-file is given as poporder.txt
+#Third argument is the populations.txt. Example is given as Population_file2.txt
+#Fourth argument is optional. It is the path to the directory containing the scripts needed for the pipeline:
+#parseVCF.py available at: https://github.com/simonhmartin/genomics_general/tree/master/VCF_processing
+#ABBA_BABA.v1.pl available at: https://github.com/owensgl/abba_baba
+#ABBA_out_blocker_5Mb.pl available at: https://github.com/owensgl/abba_baba
+#ABBA_out_blocker.pl available at: https://github.com/owensgl/abba_baba
+#convertDoGeno2egglib.pl available at: https://github.com/owensgl/abba_baba
+#countbadcolumns.pl available at: https://github.com/owensgl/pop_gen
+#Jackknife_ABBA_pipe.R available at: https://github.com/owensgl/abba_baba
+#ABBA_pvalue.R available at: https://github.com/owensgl/abba_baba
 
 
-python parseVCF.py -i solanum.fb.vcf.gz > solanum.fb.geno
-
-sed -i 's/|//g' solanum.fb.geno
-
-sed -i '1d' solanum.fb.geno
-
-perl convertDoGeno2egglib.pl solanum.fb.geno > solanum.fb.geno.txt
+#available at: https://github.com/owensgl/abba_baba
 
 
-cat header.txt solanum.fb.geno.txt > $OUTPUT/solanum.fb_WH.geno
+FILE=${1%.gz}
+FILE=${file%.vcf}
 
-mv *pl $OUTPUT
+if [ -z "$3" ]
+  then
+    script_dir=$pwd
+  else
+    script_dir=$3
+fi
 
-cd $OUTPUT
-pwd
-ls
+python $script_dir/parseVCF.py -i $FILE.vcf.gz > $FILE.geno
+
+sed -i 's/|//g' $FILE.geno
+
+sed -i '1d' $FILE.geno
+
+perl $script_dir/convertDoGeno2egglib.pl $FILE.geno > $FILE.geno.txt
+
+cat header.txt $FILE.geno.txt > $FILE"_WH.geno"
 
 echo "############## Start ABBA BABA############"
+$END=$(cat $2 | wc -l)
 
-for i in {1..20}
+for (( i=1; i<=$END; i++ ))
 do
-        perl ABBA_BABA.v1.pl solanum.fb_WH.geno ../Population_file2.txt ../poporder/Poporder_$i.txt > D_fd_Fhom_ANGSD_$i.txt
+poporder=$(head -n $2 | tail -n)
+        perl $script_dir/ABBA_BABA.v1.pl $FILE"_WH.geno" $3 $poporder > D_fd_Fhom_ANGSD_$i.txt
 
-perl ABBA_out_blocker_5Mb.pl D_fd_Fhom_ANGSD_$i.txt > D_fd_Fhom_ANGSD_block_$i.txt
+perl $script_dir/ABBA_out_blocker_5Mb.pl D_fd_Fhom_ANGSD_$i.txt > D_fd_Fhom_ANGSD_block_$i.txt
 
-Rscript ../Jackknife_ABBA_pipe2.R D_fd_Fhom_ANGSD_block_$i.txt D_fd_Fhom_ANGSD_jackknife_$i.txt
+Rscript $script_dir/Jackknife_ABBA_pipe.R D_fd_Fhom_ANGSD_block_$i.txt D_fd_Fhom_ANGSD_jackknife_$i.txt
 
 tail -n 3 D_fd_Fhom_ANGSD_$i.txt > tmp1
 tail -n 3 D_fd_Fhom_ANGSD_jackknife_$i.txt > tmp2
@@ -34,11 +56,23 @@ paste tmp1 tmp2 > D_fd_Fhom_ANGSD_2_$i.txt
 rm tmp1
 rm tmp2
 
-Rscript ../ABBA_pvalue.R D_fd_Fhom_ANGSD_2_$i.txt D_fd_Fhom_ANGSD_final_$i.txt
+Rscript $script_dir/ABBA_pvalue.R D_fd_Fhom_ANGSD_2_$i.txt D_fd_Fhom_ANGSD_final_$i.txt
 done
 
 echo "############# Finished ABBA BABA #########"
 
-../extract_zscore.sh
-mkdir site_windows
-cd site_windows
+#Summarize the D and Z-score for all the combinations in Zscore.txt
+echo -e "Poporder_Nr\tCombination\tMean_D\tStd_D\tZ_score\tp_value" > header
+for (( i=1; i<=$END; i++ ))
+do
+        echo "$i" > number
+cut -f1 ./poporder/Poporder_$i.txt | tr '\n' '\t' | sed 's/\t$//' | tr '\t' _ > combination
+cut -f3-6 D_fd_Fhom_ANGSD_final_$i.txt | head -n1 > values
+paste number combination values >> sample
+rm combination
+rm values
+rm number
+done
+cat header sample > Zscore.txt
+rm header
+rm sample
