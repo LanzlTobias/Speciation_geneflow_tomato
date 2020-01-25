bed5cfe1e967702377522d5370d650a48ee58a44
Tobias Lanzl <60113807+LanzlTobias@users.noreply.github.com>
2020-01-25 19:10:58 +0100

    Update and rename extract_zscore.sh to Simulations.sh

--- Simulations/Simulations.sh
+++ Simulations/Simulations.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+./H3toH2.sh
+./H3toH2H1.sh
+./H3toH2-lm.sh
+./H3toH2-lm-early.sh
+./H3toH2-admix.sh
+./H3toH2-admix-early.sh
+./H3toH2-cm.sh
+./nomig.sh
+./nomig-div.sh
+
 echo -e "Simulation\tMean_D\tStd_D\tZ_score\tp-value" > header
 for i in $(ls *final*txt)
 do
@@ -13,3 +23,6 @@ done
 cat header sample > Zscore_sim.txt
 rm header
 rm sample
+
+tar -czf D_sim_files.tar.gz D*txt
+rm D*txt
