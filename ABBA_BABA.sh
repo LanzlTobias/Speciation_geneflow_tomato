59268504a669827e25cc73e9f6bac8e8df24ac39
Tobias Lanzl <60113807+LanzlTobias@users.noreply.github.com>
2020-02-16 15:19:41 +0100

    Update ABBA_BABA.sh

--- ABBA_BABA.sh
+++ ABBA_BABA.sh
@@ -7,9 +7,8 @@
 #Second argument is the file containing the paths to all poporder-files. Each path has to be given in a new line.
 #Example of a poporder-file is given as pop_order.txt
 #Third argument is the populations.txt. Example is given as pop_file.txt
-#Fourth argument is optional. It is the number on which poporder it should be started. For multi-threading.
-#Fifth argument is optional. It is the number on which poporder it should end. For multi-threading.
-#Sixth argument is optional. It is the path to the directory containing the scripts needed for the pipeline.
+#Fourth argument is optional. It determines if the geno file should be created or not. Default is 0 = gene file will be created, 1 = geno file will not be created
+#Required scripts:
 #parseVCF.py available at: https://github.com/simonhmartin/genomics_general/tree/master/VCF_processing
 #ABBA_BABA.v1.pl available at: https://github.com/owensgl/abba_baba
 #ABBA_out_blocker_5Mb.pl available at: https://github.com/owensgl/abba_baba
@@ -20,57 +19,42 @@
 #ABBA_pvalue.R available at: https://github.com/owensgl/abba_baba
 
 FILE=${1%.gz}
-FILE=${file%.vcf}
+FILE=${FILE%.vcf}
 
-if [ -z "$3" ]
-  then
-    script_dir=$pwd
-  else
-    script_dir=$3
-fi
 
-if test "$5" == "1"
+if test "$4" == "0"
   then
 
-python $script_dir/parseVCF.py -i $FILE.vcf.gz > $FILE.geno
+python parseVCF.py -i $FILE.vcf.gz > $FILE.geno
 
 sed -i 's/|//g' $FILE.geno
-
+sed -i 's/\///g' $FILE.geno
 sed -i '1d' $FILE.geno
 
-perl $script_dir/convertDoGeno2egglib.pl $FILE.geno > $FILE.geno.txt
+echo -e "Chromosome\tPosition" > tmp1
+
+bcftools query -l solanum.fb.dp.vcf.gz | tr "\n" "\t" > tmp2
+
+paste tmp1 tmp2
+
+perl convertDoGeno2egglib.pl $FILE.geno > $FILE.geno.txt
 
 cat header.txt $FILE.geno.txt > $FILE"_WH.geno"
 
-rm $FILE.geno.txt $FILE.geno
 fi
 
 echo "############## Start ABBA BABA############"
 END=$(cat $2 | wc -l)
 
-if [ -z "$5" ]
-  then
-    START=1
-  else
-    START=$5
-fi
 
-if [ -z "$6" ]
-  then
-   END=$(cat $2 | wc -l)
-  else
-   END=$6
-fi
-    
-
-for (( i=$START; i<=$END; i++ ))
+for (( i=1; i<=$END; i++ ))
 do
 poporder=$(head -n $i $2 | tail -n 1)
-        perl $script_dir/ABBA_BABA.v1.pl $FILE"_WH.geno" $3 $poporder > D_fd_Fhom_ANGSD_$i.txt
+        perl ABBA_BABA.v1.pl $FILE"_WH.geno" $3 $poporder > D_fd_Fhom_ANGSD_$i.txt
 
-perl $script_dir/ABBA_out_blocker_5Mb.pl D_fd_Fhom_ANGSD_$i.txt > D_fd_Fhom_ANGSD_block_$i.txt
+perl ABBA_out_blocker_5Mb.pl D_fd_Fhom_ANGSD_$i.txt > D_fd_Fhom_ANGSD_block_$i.txt
 
-Rscript $script_dir/Jackknife_ABBA_pipe.R D_fd_Fhom_ANGSD_block_$i.txt D_fd_Fhom_ANGSD_jackknife_$i.txt
+Rscript Jackknife_ABBA_pipe.R D_fd_Fhom_ANGSD_block_$i.txt D_fd_Fhom_ANGSD_jackknife_$i.txt
 
 tail -n 3 D_fd_Fhom_ANGSD_$i.txt > tmp1
 tail -n 3 D_fd_Fhom_ANGSD_jackknife_$i.txt > tmp2
@@ -79,22 +63,23 @@ paste tmp1 tmp2 > D_fd_Fhom_ANGSD_2_$i.txt
 rm tmp1
 rm tmp2
 
-Rscript $script_dir/ABBA_pvalue.R D_fd_Fhom_ANGSD_2_$i.txt D_fd_Fhom_ANGSD_final_$i.txt
+Rscript ABBA_pvalue.R D_fd_Fhom_ANGSD_2_$i.txt D_fd_Fhom_ANGSD_final_$i.txt
 done
 
 echo "############# Finished ABBA BABA #########"
 
 #Summarize the D and Z-score for all the combinations in Zscore.txt
 echo -e "Poporder_Nr\tCombination\tMean_D\tStd_D\tZ_score\tp_value" > Zscore.txt
-for (( i=$START; i<=$END; i++ ))
+for (( i=1; i<=$END; i++ ))
 do
         echo "$i" > number
-        poporder=$(head -n $2 | tail -n)
+        pop_file=$(head -n $2 | tail -n)
+        poporder=$(cat pop_file)
 cut -f1 $poporder | tr '\n' '\t' | sed 's/\t$//' | tr '\t' _ > combination
 cut -f3-6 D_fd_Fhom_ANGSD_final_$i.txt | head -n1 > values
 paste number combination values >> sample
 
-cat sample >> Zscore_$START"_"$END.txt
+cat sample >> Zscore.txt
 done
 rm combination
 rm values
